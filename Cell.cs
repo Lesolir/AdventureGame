216843aa7446d7309d7f114101331bcb36da5477
Lesolir <56299150+Lesolir@users.noreply.github.com>
2020-01-07 11:11:41 +0100

    Spelet

---
+++ Cell.cs
@@ -0,0 +1,177 @@
+using System;
+
+namespace KeyQuest
+{
+    class Cell
+    {
+        private string landType = "Dark Space";
+        private int mobs = 0;
+        private Mob[] mob = new Mob[3];
+        private int key = 0;
+        private int potion = 0;
+        private int weaponUpgrade = 0;
+        private int visited = 0;
+
+
+        public Cell()
+        {}
+
+
+        public void SetLandType()
+        {
+            // Forrest, Cave, Marshes, Tundra, Mountains, Dessert, Dark Woods, Ravine, Snowy Mountains
+            // Steppes, Abandoned Mine, Old Fortress, Swamp, Haunted Village, Friendly Village, Town, City, Capital
+            // Silent Castle, Farm, Green Hills, 
+            string landType = "";
+            Random random = new Random();
+            int land = random.Next(0,26);
+            switch (land)
+            {
+                case 0:
+                    landType = " a forrest";
+                    break;
+                case 1:
+                    landType = " a cave";
+                    break;
+                case 2:
+                    landType = " the marshes";
+                    break;
+                case 3:
+                    landType = " the tundra";
+                    break;
+                case 4:
+                    landType = " the mountains";
+                    break;
+                case 5:
+                    landType = " the dessert";
+                    break;
+                case 6:
+                    landType = " a dark wood";
+                    break;
+                case 7:
+                    landType = " a ravine";
+                    break;
+                case 8:
+                    landType = " the snowy mountains";
+                    break;
+                case 9:
+                    landType = " the steppes";
+                    break;
+                case 10:
+                    landType = " an abandoned mine";
+                    break;
+                case 11:
+                    landType = " an old fortress";
+                    break;
+                case 12:
+                    landType = " a swamp";
+                    break;
+                case 13:
+                    landType = " a haunted village";
+                    break;
+                case 14:
+                    landType = " a friendy village";
+                    break;
+                case 15:
+                    landType = " a town";
+                    break;
+                case 16:
+                    landType = " a city";
+                    break;
+                case 17:
+                    landType = " a great capital";
+                    break;
+                case 18:
+                    landType = " a silent castle";
+                    break;
+                case 19:
+                    landType = " a farm";
+                    break;
+                case 20:
+                    landType = " the green hills";
+                    break;
+                case 21:
+                    landType = " a graveyard";
+                    break;
+                case 22:
+                    landType = " an evil ruin";
+                    break;
+                case 23:
+                    landType = " a ruin";
+                    break;
+                case 24:
+                    landType = " the cliffs by the ocean";
+                    break;
+                case 25:
+                    landType = " a lake";
+                    break;
+            }
+            this.landType = landType;
+        }
+        public string GetLandType()
+        {
+            string landType = this.landType;
+            return landType;
+        }
+        public void SetMob()
+        {
+            Random random = new Random();
+            int mob = random.Next(1,4);
+            this.mobs = mob;
+            
+            Mob[] mobs = new Mob[mob];
+            for(int i = 0; i < mob; i++)
+            {
+                mobs[i] = new Mob();
+                mobs[i].SetName();
+                this.mob[i] = mobs[i];
+            }
+        }
+        public int GetMobs()
+        {
+            int mob = this.mobs;
+            return mob;
+        }
+        public Mob GetMob(int i)
+        {
+            mob[i] = this.mob[i];
+            return mob[i];
+        }
+        public void SetKey(int key)
+        {
+            this.key = key;
+        }
+        public int GetKey()
+        {
+            int key = this.key;
+            return key;
+        }
+        public void SetPotion(int potion)
+        {
+            this.potion = potion;
+        }
+        public int GetPotion()
+        {
+            int potion = this.potion;
+            return potion;
+        }
+        public void SetWeaponUpgrade(int weaponUpgrade)
+        {
+            this.weaponUpgrade = weaponUpgrade;
+        }
+        public int GetWeaponUpgrade()
+        {
+            int weaponUpgrade = this.weaponUpgrade;
+            return weaponUpgrade;
+        }
+        public void SetVisited(int visited)
+        {
+            this.visited = visited;
+        }
+        public int GetVisited()
+        {
+            int visited = this.visited;
+            return visited;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,323 @@
+﻿using System;
+
+namespace KeyQuest
+{
+    class Program
+    {
+        // This states the version of the program
+        static void BuildVersion()
+        {
+            Console.SetCursorPosition(Console.WindowWidth - 8, Console.WindowHeight - 2);
+            Console.WriteLine("V. 0.01");
+        }
+        //This is the main menu of the program
+        static int MainMenu()
+        {
+            string saved = "0";
+            try
+            {
+                saved = System.IO.File.ReadAllText(@"SavedGames.txt");
+            }
+            catch
+            {
+                System.IO.File.WriteAllText(@"SavedGames.txt", saved);
+            }
+            int savedGames = int.Parse(saved);
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth - 9) / 2, Console.CursorTop + 6);
+            Console.WriteLine("Main Menu");
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 16, Console.CursorTop + 2);
+            Console.WriteLine("1. New Game (free slots: {0}/10)", 10 - savedGames);
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 16, Console.CursorTop);
+            Console.WriteLine("2. Load Game (saved games: {0}/10)", savedGames);
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 16, Console.CursorTop);
+            Console.WriteLine("3. Exit");
+            Console.SetCursorPosition((Console.WindowWidth - 47) / 2, Console.CursorTop + 2);
+            Console.WriteLine("Please select an option and confirm with ENTER");
+            return savedGames;
+        }
+        // This is the Create new game menu
+        static int NewGame(Hero[] hero)
+        {
+            //Hero[] hero = new Hero[10];
+            string saved = System.IO.File.ReadAllText(@"SavedGames.txt");
+            int savedGames = int.Parse(saved);
+            int currentGame = savedGames;
+            hero[savedGames] = new Hero();
+
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 9, Console.CursorTop);
+            Console.WriteLine("Character Creation");
+            Console.Write("\n\n\nPlease enter the name of your hero (confirm with ENTER):");
+            string answer = Console.ReadLine();
+            hero[savedGames].SetName(answer);
+
+            HeroInfo(hero, ref currentGame);            
+            Console.WriteLine("\n\nStart Game: Press ENTER");
+            Console.ReadLine();
+
+            savedGames++;
+            savedGames.ToString(saved);
+            System.IO.File.WriteAllText(@"SavedGames.txt", saved);
+            return currentGame;
+        }
+        // This displays hero info to the console
+        static void HeroInfo(Hero[] hero, ref int currentGame)
+        {
+            //string saved = System.IO.File.ReadAllText(@"SavedGames.txt");
+            //int savedGames = int.Parse(saved);
+            string name = hero[currentGame].GetName();
+            int level = hero[currentGame].GetLevel();
+            int xp = hero[currentGame].GetXP();
+            int health = hero[currentGame].GetHealth();
+            int attack = hero[currentGame].GetAttack();
+            int keys = hero[currentGame].GetKeys();
+            int potion = hero[currentGame].GetPotion();
+            int weaponUpgrade = hero[currentGame].GetWeaponUpgrade();
+            int positionX = hero[currentGame].GetPositionX();
+            int positionY = hero[currentGame].GetPositionY();
+
+            Console.WriteLine("\nName: {0}\nLevel: {1}\nXP: {2}\nHealth: {3}\nAttack: {4}\nKeys: {5}\nPotion: {6}\nWeapon Upgrade: {7}\nPosition: X{8} || Y{9}",
+                name, level, xp, health, attack, keys, potion, weaponUpgrade, positionX, positionY);
+        }
+        // This builds the world
+        static void BuildNewWorld(Cell[,] cell)
+        {
+            //Cell[,] cell = new Cell[10,10];
+            
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 7, Console.CursorTop + 10);
+            Console.WriteLine("Building World");
+            
+            for(int y = 0; y < 10; y++)
+            {
+                
+                for(int x = 0; x < 10; x++)
+                {
+                    cell[x,y] = new Cell();
+                    cell[x,y].SetLandType();
+                    System.Threading.Thread.Sleep(10);
+                    Console.Write(".");
+                }
+
+            }
+
+            PlaceMobs(cell);
+            PlaceKeys(cell);
+
+        }
+        // This places mobs in 10 cells
+        static void PlaceMobs(Cell[,] cell)
+        {
+            Random random = new Random();
+            int x = 0, y = 0, mob = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                x = random.Next(0, 10);
+                y = random.Next(0, 10);
+                mob = cell[x, y].GetMobs();
+                if (mob == 0)
+                    cell[x, y].SetMob();
+                else
+                    i--;
+            }
+        }
+        // This places keys in 10 cells
+        static void PlaceKeys(Cell[,] cell)
+        {
+            Random random = new Random();
+            int x = 0, y = 0, key = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                x = random.Next(0, 10);
+                y = random.Next(0, 10);
+                key = cell[x, y].GetKey();
+                if (key == 0)
+                    cell[x, y].SetKey(1);
+                else
+                    i--;
+            }
+        }
+        // This is the load game menu
+        static int LoadGame()
+        {
+            int currentGame = 0;
+            return currentGame;
+        }
+        // This loads a saved world
+        static void LoadWorld(Cell[,] cell)
+        {
+
+        }
+        // This is the action menu
+        static int HeroAction()
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Go north");
+            Console.WriteLine("2. Go east");
+            Console.WriteLine("3. Go south");
+            Console.WriteLine("4. Go west");
+            Console.WriteLine("\n5. Watch the map");
+            Console.WriteLine("6. Drink a potion");
+            Console.WriteLine("7. Upgrade weapon");
+            Console.WriteLine("\n8. Exit to main menu");
+            Console.WriteLine("\nPlease select one of the above alternatives\nConfirm with ENTER");
+
+            //int.TryParse(Console.ReadLine(), out int answer);
+            int answer;
+
+            while(!int.TryParse(Console.ReadLine(), out answer) || answer < 1 || answer > 8)
+                ErrorInput();
+            
+            return answer;
+        }
+        // This is the error input message
+        static void ErrorInput()
+        {
+            Console.SetCursorPosition((Console.WindowWidth) / 2 - 21, Console.CursorTop + 2);
+            Console.WriteLine("ERROR! Invalid selection, please try again.");
+            Console.SetCursorPosition((Console.WindowWidth) / 2 - 11, Console.CursorTop + 1);
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+        }
+        // ============================================================================================
+        // ============================================================================================
+        // MAIN
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition((Console.WindowWidth - 8) / 2, Console.CursorTop + 6);
+            Console.WriteLine("KeyQuest");
+            Console.SetCursorPosition((Console.WindowWidth - 23) / 2, Console.CursorTop + 4);
+            Console.WriteLine("Press ENTER to continue");
+            BuildVersion();
+            Console.ReadLine();
+
+            Console.Clear();
+            Console.Write("\n\n\n\n\n\n\t\tLoading awesome adventures.");
+            for(int i = 0; i < 10; i++)
+            {
+                System.Threading.Thread.Sleep(200);
+                Console.Write(".");
+            }
+
+            string choice = "0";
+            Hero[] hero = new Hero[10];
+            Cell[,] cell = new Cell[10, 10];
+            bool exit = false;
+            while(!exit)
+            {
+                int savedGames = MainMenu();
+                BuildVersion();
+                bool runGame = false, newGame = false, loadGame = false;
+                int currentGame = 0;
+                
+                Console.SetCursorPosition((Console.WindowWidth) / 2 - 7, Console.CursorTop - 12);
+                choice = Console.ReadLine();
+
+                if(choice == "1")
+                {
+                    if(savedGames < 10)
+                    {
+                        currentGame = NewGame(hero);
+                        runGame = true;
+                        newGame = true;
+                    }
+                    else
+                    {
+                        ErrorInput();
+                        Console.WriteLine("There are no free slots for new games");
+                        Console.WriteLine("Please delete a saved game to be able to create a new game");
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                    }
+                }
+                else if(choice == "2")
+                {
+                    if (savedGames > 0)
+                    {
+                        currentGame = LoadGame();
+                        runGame = true;
+                        loadGame = true;
+                    }
+                    else
+                    {
+                        ErrorInput();
+                        Console.WriteLine("There are no saved games to load");
+                        Console.WriteLine("Please create a new game to play");
+                        Console.WriteLine("Press ENTER to continue");
+                        Console.ReadLine();
+                    }
+                }
+                else if(choice == "3")
+                {
+                    exit = true;
+                }
+                else
+                {
+                    ErrorInput();
+                }
+
+                if(runGame)
+                {
+                    if(newGame)
+                        BuildNewWorld(cell);
+                    else if(loadGame)
+                        LoadWorld(cell);
+
+                    int answer = 0;
+                    while(!exit)
+                    {
+                        Console.Clear();
+                        HeroInfo(hero, ref currentGame);
+                        answer = HeroAction();
+
+                        switch(answer)
+                        {
+                            case 1:
+                                hero[currentGame].SetPositionY(-1);
+                                break;
+                            case 2:
+                                hero[currentGame].SetPositionX(+1);
+                                break;
+                            case 3:
+                                hero[currentGame].SetPositionY(+1);
+                                break;
+                            case 4:
+                                hero[currentGame].SetPositionX(-1);
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                break;
+                            case 7:
+                                break;
+                            case 8:
+                                exit = true;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    exit = false;
+                }
+            }
+
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth - 21) / 2, Console.CursorTop + 6);
+            Console.WriteLine("Thank you for playing!");
+            Console.SetCursorPosition((Console.WindowWidth - 19) / 2, Console.CursorTop + 4);
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth / 2) - 30, Console.CursorTop + 10);
+            Console.Write("Chasing away the last monsters.");
+            for(int i = 0; i < 15; i++)
+            {
+                System.Threading.Thread.Sleep(200);
+                Console.Write(".");
+                if(i == 4)
+                    Console.Write("Shoo shoo.");
+            }
+        }
+    }
+}
