6cc1b89507f736311e7b4358e7ad6a77975203c3
Lesolir <56299150+Lesolir@users.noreply.github.com>
2020-01-08 09:11:40 +0100

    Fixat uppdateringen av savedgames

--- Program.cs
+++ Program.cs
@@ -66,7 +66,7 @@ namespace KeyQuest
 
 
             savedGames++;
-            savedGames.ToString(saved);
+            saved = savedGames.ToString();
             System.IO.File.WriteAllText(@"SavedGames.txt", saved);
             return currentGame;
         }
